928afe55699faed074e0a32efda6b0dafba8058f
Wizard Nate <nathanielorozco42@gmail.com>
2024-06-22 20:56:17 -0500

    Updated Art

--- scripts/tesla_coil.gd
+++ scripts/tesla_coil.gd
@@ -1,12 +1,8 @@
 extends Node2D
 
-
-
-func _on_area_2d_area_entered(area):
-	pass
-
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	if area.is_in_group("water"):
 		print("ZAP!")
-		#
+		animated_sprite.play("destroyed")
