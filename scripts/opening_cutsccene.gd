de0201aa08edb8ff9bce9f1efd6afc3b46f640fc
Wizard Nate <nathanielorozco42@gmail.com>
2024-06-23 15:12:04 -0500

    about to test build

--- scripts/opening_cutsccene.gd
+++ scripts/opening_cutsccene.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
 func _on_animated_sprite_2d_animation_finished():
-	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+	pass
+	#get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
