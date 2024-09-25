bfe62c4d39b8d3b710d443f0b2764750345318d6
Wizard Nate <nathanielorozco42@gmail.com>
2024-09-25 16:25:42 -0500

    Final assets
    More assets that i never realized that they didnt make it into the git. This game is awesome btw

--- scripts/opening_cutsccene.gd
+++ scripts/opening_cutsccene.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
-func _on_animated_sprite_2d_animation_finished():
-	pass
-	#get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+func _on_animation_player_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
